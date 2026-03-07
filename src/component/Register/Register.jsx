9f4f287d70c5fb101f5e8975a7d06ebaea8fb46f
Raju-ahamed <rajuahamed7320@gmail.com>
2026-03-07 15:05:52 +0600

    icon

--- src/component/Register/Register.jsx
+++ src/component/Register/Register.jsx
@@ -1,18 +1,35 @@
 import { createUserWithEmailAndPassword } from 'firebase/auth';
 import React, { useState } from 'react';
 import { auth } from '../../firebase.init';
+import { FaEye, FaEyeSlash } from 'react-icons/fa';
 
 
 const Register = () => {
     const [errorMsg, setErrorMsg] = useState("");
-    const [sucses,setSucses] = useState(false);
+    const [sucses, setSucses] = useState(false);
     const handleSubmit = (e) => {
         e.preventDefault();
-        
+
         const email = e.target.email.value;
         const password = e.target.password.value;
         setSucses(false);
         setErrorMsg('')
+        const passwordExUp = /^(?=.*[A-Z]).+$/;
+        const passwordExlow = /^(?=.*[a-z]).+$/;
+        const passwordExN = /^(?=.*\d).{8,}$/
+        if (passwordExUp.test(password) === false) {
+            setErrorMsg('Enter One Uper case');
+            return;
+        }
+        else if (passwordExlow.test(password) === false) {
+            setErrorMsg('Enter One Lower case');
+            return;
+        }
+        else if (passwordExN.test(password) === false) {
+            setErrorMsg('Enter One Number');
+            return;
+        }
+
         createUserWithEmailAndPassword(auth, email, password).then(result => {
             console.log(result);
             setSucses(true);
@@ -22,6 +39,8 @@ const Register = () => {
         })
 
     }
+    const [showPass, setShowPass] = useState(false);
+
     return (
 
         <div className="hero bg-base-200 min-h-screen">
@@ -36,7 +55,18 @@ const Register = () => {
                             <label className="label">Email</label>
                             <input type="email" name='email' className="input" placeholder="Email" />
                             <label className="label">Password</label>
-                            <input type="password" name='password' className="input" placeholder="Password" />
+                            <div className='relative'>
+                                <input
+                                    type={
+                                        showPass ? "text" : "password"
+                                    }
+                                    name='password'
+                                    className="input"
+                                    placeholder="Password" />
+                                <p onClick={() => setShowPass(!showPass)} className='btn btn-xs top-2 right-4 absolute'>{
+                                    showPass ? <FaEyeSlash/>:<FaEye/>
+                                }</p>
+                            </div>
                             <div><a className="link link-hover">Forgot password?</a></div>
                             <button className="btn btn-neutral mt-4">Register</button>
                         </form>
